b4ff5b394209f7da9aedbba2319a5d6b587b69c3
krusher1911 <ovatsug60@hotmail.com>
2016-04-19 22:39:16 -0300

    inicial

---
+++ menu.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    var menu_princ="#menu1";
+
+    
+    $('botaoMenu').click(function(){
+        $(menu_princ).append('<div id="sobre" class="botaoMenu">teste</div>');
+        $('botaoMenu').fadeOut("slow");
+        
+        
+    })
+    
+    
+ $(".botaoMenu").click(function(){
+    var x = this.id;
+    
+    $("html").fadeOut()
+    
+    setTimeout(function(){redirMenu(x)}, 250);
+
+        
+ });
+
+function redirMenu(x){
+
+    switch (x){
+            case "modo_zen":
+                window.location.href = 'modo_zen.html';
+                break;
+            case "arcade":
+                window.location.href = 'modo_arcade.html';
+                break;
+            default:
+                window.location.href = 'index.html';
+                break;            
+    }
+}
+
+})
